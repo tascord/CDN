0153424002665a3e79b09c02fd07979d6ae1ee9e
tascord <theactivesquare@gmail.com>
2020-11-12 07:55:01 +1100

    File upload

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ a.set('view engine', 'ejs');
 a.listen(3000, () => console.log('Started!'));
 
 const f = require('fs');
-const m = require('multiparty');
+const m = require('multer');
+
+const u = m({ storage: require('./storage')({ destination: (r, f, c) => { c(null, 'files'); } }) })
+
+const token = process.env.TOKEN || 'something secure lol';
 
 a.get('*', (i, o) => {
 
     let path = i.path.slice(1);
     if(!path) return o.render(`${__dirname}/pages/index.ejs`);
     
+    if(path === 'upload') return o.render(`${__dirname}/pages/upload.ejs`);
+
     if(path.startsWith('raw/')) {
 
         path = path.slice(4);
@@ -26,8 +32,11 @@ a.get('*', (i, o) => {
 
 });
 
-a.post('/up', (i, o) => {
+//
 
+a.post('/up', u.single('file'), (i, o) => {
 
+    console.log('okiedokei');
+    o.status(204).end();
 
 });
